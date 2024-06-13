ae7d681566890a8e25d43e06d516f471244eca33
Vika <vikapetrova8@gmail.com>
2024-06-13 21:21:34 +0300

    basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,41 @@ import s from './App.module.css';
 
 function App() {
     const [value, setValue] = useState<number>(0)
-    const onClickIncrement = () => {setValue(value + 1)}
-    const onClickReset = () => {setValue(0)}
+    const onClickIncrement = () => {
+        setValue(value + 1)
+    }
+    const onClickReset = () => {
+        setValue(0)
+    }
 
-  return (
-    <div className={s.counter}>
-      <div className={s.value}>
-          {value}
-      </div>
-      <div className={s.btnWrapper}>
-        <button onClick={onClickIncrement}>inc</button>
-        <button onClick={onClickReset}>reset</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={s.wrapper}>
+            <div className={s.settings}>
+                <div className={s.valuesWrapper}>
+                    <label htmlFor="">
+                        max value:
+                        <input type="text"/>
+                    </label>
+                    <label htmlFor="">
+                        start value:
+                        <input type="text"/>
+                    </label>
+                </div>
+                <div>
+                    <button>set</button>
+                </div>
+            </div>
+            <div className={s.counter}>
+                <div className={s.value}>
+                    {value}
+                </div>
+                <div className={s.btnWrapper}>
+                    <button onClick={onClickIncrement}>inc</button>
+                    <button onClick={onClickReset}>reset</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
