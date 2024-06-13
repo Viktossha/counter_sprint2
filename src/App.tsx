6854eedc9607ee92656c592d2c130d98c6b7f5fc
Vika <vikapetrova8@gmail.com>
2024-06-13 21:13:58 +0300

    simple counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import s from './App.module.css';
 
 function App() {
+    const [value, setValue] = useState<number>(0)
+    const onClickIncrement = () => {setValue(value + 1)}
+    const onClickReset = () => {setValue(0)}
+
   return (
-    <div className="App">
+    <div className={s.counter}>
+      <div className={s.value}>
+          {value}
+      </div>
+      <div className={s.btnWrapper}>
+        <button onClick={onClickIncrement}>inc</button>
+        <button onClick={onClickReset}>reset</button>
+      </div>
     </div>
   );
 }
